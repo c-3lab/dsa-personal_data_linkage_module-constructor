408199526074da5435eeb20f8177ca15dfd120c3
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-01-15 17:27:41 +0900

    add scripts and CFn templates to create & delete ACM & WebACL Stack

---
+++ 12_create_acm_and_webacl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source .env
+
+echo "ACM_STACK_NAME=$ACM_STACK_NAME"
+echo "WEBACL_STACK_NAME=$WEBACL_STACK_NAME"
+echo "DOMAIN=$DOMAIN"
+
+ZONE_STR=$(aws route53 list-hosted-zones --query "HostedZones[?Name=='${DOMAIN}.'].Id" --output text)
+SPLITTED=(${ZONE_STR//\// })
+ZONE_ID=${SPLITTED[1]}; echo "ZONE_ID=$ZONE_ID"
+
+# create ACM
+aws cloudformation create-stack --region ap-northeast-1 --stack-name $ACM_STACK_NAME --template-body file://CFn/acm.yaml \
+  --parameters ParameterKey=DomainName,ParameterValue=$DOMAIN \
+               ParameterKey=HostedZoneId,ParameterValue=$ZONE_ID
+aws cloudformation wait stack-create-complete --stack-name $ACM_STACK_NAME
+aws cloudformation describe-stacks --stack-name $ACM_STACK_NAME
+
+
+# create WebACL
+MY_PUBLIC_IP="$(curl inet-ip.info)"; echo "MY_PUBLIC_IP=$MY_PUBLIC_IP"
+
+aws cloudformation create-stack --region ap-northeast-1 --stack-name $WEBACL_STACK_NAME --template-body file://CFn/webacl.yaml \
+  --parameters ParameterKey=AllowedIP1,ParameterValue=$MY_PUBLIC_IP
+aws cloudformation wait stack-create-complete --stack-name $WEBACL_STACK_NAME
+aws cloudformation describe-stacks --stack-name $WEBACL_STACK_NAME
+
+# replace ingress placeholders
+SED="docker run --rm -v $(pwd)/src:/opt/src -w /opt busybox:1.36 sed"
+
+ACM_ARN=$(aws cloudformation describe-stacks --stack-name $ACM_STACK_NAME --query "Stacks[0].Outputs[?ExportName=='$ACM_STACK_NAME-acm'].OutputValue" --output text); echo "ACM_ARN=$ACM_ARN"
+WEBACL_ARN=$(aws cloudformation describe-stacks --stack-name $WEBACL_STACK_NAME --query "Stacks[0].Outputs[?ExportName=='$WEBACL_STACK_NAME-webacl'].OutputValue" --output text); echo "WEBACL_ARN=$WEBACL_ARN"
+
+$SED -i -e "s|^    alb.ingress.kubernetes.io/certificate-arn: .*$|    alb.ingress.kubernetes.io/certificate-arn: $ACM_ARN|g" src/manifest/eks/ingress/pxr-ingress.yaml
+$SED -i -e "s|^    alb.ingress.kubernetes.io/wafv2-acl-arn: .*$|    alb.ingress.kubernetes.io/wafv2-acl-arn: $WEBACL_ARN|g" src/manifest/eks/ingress/pxr-ingress.yaml
+$SED -i -e "/alb.ingress.kubernetes.io\/security-groups/d" src/manifest/eks/ingress/pxr-ingress.yaml
+$SED -i -e "s/domain.com/$DOMAIN/g" src/manifest/eks/ingress/pxr-ingress.yaml
+
---
+++ 94_delete_acm_and_webacl.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source .env
+
+echo "ACM_STACK_NAME=$ACM_STACK_NAME"
+echo "WEBACL_STACK_NAME=$WEBACL_STACK_NAME"
+
+# delete WebSCL stack
+aws cloudformation delete-stack --stack-name $WEBACL_STACK_NAME
+aws cloudformation wait stack-delete-complete --stack-name $WEBACL_STACK_NAME
+
+# delete ACM stack
+aws cloudformation delete-stack --stack-name $ACM_STACK_NAME
+aws cloudformation wait stack-delete-complete --stack-name $ACM_STACK_NAME
+
+# delete a Route53 Record used ACM Validation
+ACM_ARN=$(aws cloudformation describe-stacks --stack-name $ACM_STACK_NAME --query "Stacks[0].Outputs[?ExportName=='$ACM_STACK_NAME-acm'].OutputValue" --output text); echo "ACM_ARN=$ACM_ARN"
+
+RECORD_NAME=$(aws acm describe-certificate --certificate-arn ${ACM_ARN} --query "Certificate.DomainValidationOptions[0].ResourceRecord.Name" --output text); echo "RACORD_NAME=$RECORD_NAME"
+RECORD_VALUE=$(aws acm describe-certificate --certificate-arn ${ACM_ARN} --query "Certificate.DomainValidationOptions[0].ResourceRecord.Value" --output text); echo "RECORD_VALUE=$RECORD_VALUE"
+
+# set validate record
+ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[?Name=='${DOMAIN}.'].Id" --output text); echo "ZONE_ID=$ZONE_ID"
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch \
+"{
+  \"Changes\": [
+    {
+      \"Action\": \"DELETE\",
+      \"ResourceRecordSet\": {
+        \"Name\": \"$RECORD_NAME\",
+        \"Type\": \"CNAME\",
+        \"TTL\": 300,
+        \"ResourceRecords\": [{\"Value\": \"$RECORD_VALUE\"}]
+      }
+    }
+  ]
+}"
+
