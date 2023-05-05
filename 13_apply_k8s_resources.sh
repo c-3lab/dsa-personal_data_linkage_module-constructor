228e26ffd4a50f4dc707f1a494a22f4b92d9cc0c
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-05-05 20:47:08 +0900

    change the manifests' path, and add some commands to wait for the pods to be active

--- 13_apply_k8s_resources.sh
+++ 13_apply_k8s_resources.sh
@@ -7,10 +7,14 @@ cp cert/client-ca.crt cert/client.crt
 kubectl -n $NAMESPACE create secret generic nginx-secret --from-file=cert/server.crt --from-file=cert/server.key --from-file=cert/client.crt --from-file=cert/client.key
 
 # apply deployments
-kubectl apply -f src/manifest/eks/services -R
-kubectl apply -f src/manifest/eks/configmap -R
-kubectl apply -f src/manifest/eks/deployment/root-deployment.yaml
-kubectl apply -f src/manifest/eks/deployment/region000001-deployment.yaml
-kubectl apply -f src/manifest/eks/deployment/application000001-deployment.yaml
-kubectl apply -f src/manifest/eks/ingress/pxr-ingress.yaml
+kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/services -R
+kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/configmap -R
+kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/root-deployment.yaml
+kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/region000001-deployment.yaml
+kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/application000001-deployment.yaml
+kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/ingress/pxr-ingress.yaml
+
+kubectl wait -n $NAMESPACE --timeout=300s --for=condition=available deployment/root-api
+kubectl wait -n $NAMESPACE --timeout=300s --for=condition=available deployment/region000001-api
+kubectl wait -n $NAMESPACE --timeout=300s --for=condition=available deployment/application000001-api
 
