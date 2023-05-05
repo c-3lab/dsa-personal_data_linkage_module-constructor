2cfa11157e85c178ebf38a052346a7d864dbe083
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-05-05 16:07:34 +0900

    change the script to accept the postgresql version

--- 07_create_rds.sh
+++ 07_create_rds.sh
@@ -5,12 +5,14 @@ source .env
 echo "VPC_STACK_NAME=$VPC_STACK_NAME"
 echo "RDS_STACK_NAME=$RDS_STACK_NAME"
 echo "DB_NAME=$DB_NAME"
+echo "DB_VERSION=$DB_VERSION"
 
 # create vpc
 aws cloudformation create-stack --region ap-northeast-1 --stack-name $RDS_STACK_NAME --template-body file://CFn/rds.yaml \
   --parameters ParameterKey=VpcStackName,ParameterValue=$VPC_STACK_NAME \
                ParameterKey=DBName,ParameterValue=$DB_NAME \
-               ParameterKey=DBUserPassword,ParameterValue=$DB_USER_PASSWORD
+               ParameterKey=DBUserPassword,ParameterValue=$DB_USER_PASSWORD \
+               ParameterKey=DBVersion,ParameterValue=$DB_VERSION
 aws cloudformation wait stack-create-complete --stack-name $RDS_STACK_NAME
 aws cloudformation describe-stacks --stack-name $RDS_STACK_NAME
 
