316e2c2667c02e6f06c64cf10ee30f5851d76aca
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-05-05 12:15:58 +0900

    change the shell script to be able to give the x509 certification's subject as an environment variable when building pxr-certification-authority-service

--- 04_build_images.sh
+++ 04_build_images.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+source .env
+echo "CERT_PREFECTURES=$CERT_PREFECTURES"
+echo "CERT_MUNICIPALITIES=$CERT_MUNICIPALITIES"
+echo "CERT_ORGANIZATION=$CERT_ORGANIZATION"
+
 SED="docker run --rm -v $(pwd)/src:/opt/src busybox:1.36 sed"
 
 # pxr-access-control-manage-service
+mkdir -p ./src/pxr-access-control-manage-service/cert
 cp ./cert/client.key ./src/pxr-access-control-manage-service/cert/
 cp ./cert/client-ca.crt ./src/pxr-access-control-manage-service/cert/
 cp ./cert/client-ca.crt ./src/pxr-access-control-manage-service/config/default-raw.crt
@@ -11,6 +17,7 @@ $SED -i -e '$d' /opt/src/pxr-access-control-manage-service/config/default-raw.cr
 docker build -t pxr/pxr-access-control-manage-service:1.0 src/pxr-access-control-manage-service
 
 # pxr-access-control-service
+mkdir -p ./src/pxr-access-control-service/cert
 cp ./cert/client.key ./src/pxr-access-control-service/cert/
 cp ./cert/client-ca.crt ./src/pxr-access-control-service/cert/
 docker build -t pxr/pxr-access-control-service:1.0 src/pxr-access-control-service
@@ -37,9 +44,9 @@ docker build -t pxr/pxr-catalog-update-service:1.0 src/pxr-catalog-update-servic
 docker build -t pxr/pxr-certificate-manage-service:1.0 src/pxr-certificate-manage-service
 
 # pxr-certification-authority-service
-$SED -i -e "s/<prefectures>/Tokyo/" /opt/src/pxr-certification-authority-service/Dockerfile
-$SED -i -e "s/<municipalities>/Shinjuku-ku/" /opt/src/pxr-certification-authority-service/Dockerfile
-$SED -i -e "s/<organization>/pxr3, Inc./" /opt/src/pxr-certification-authority-service/Dockerfile
+$SED -i -e "s/<prefectures>/$CERT_PREFECTURES/" /opt/src/pxr-certification-authority-service/Dockerfile
+$SED -i -e "s/<municipalities>/$CERT_MUNICIPALITIES/" /opt/src/pxr-certification-authority-service/Dockerfile
+$SED -i -e "s/<organization>/$CERT_ORGANIZATION/" /opt/src/pxr-certification-authority-service/Dockerfile
 docker build -t pxr/pxr-certification-authority-service:1.0 src/pxr-certification-authority-service
 
 # pxr-ctoken-ledger-service
