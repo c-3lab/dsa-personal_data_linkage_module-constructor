e3c19d9be7e164ccb0dcefc863dfdf531abce9a6
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-05-05 21:03:57 +0900

    update manifests' path

--- 93_delete_k8s_resources.sh
+++ 93_delete_k8s_resources.sh
@@ -6,10 +6,10 @@ source .env
 kubectl -n $NAMESPACE delete secret nginx-secret
 
 # delete deployments
-kubectl delete -f src/manifest/eks/ingress/pxr-ingress.yaml
-kubectl delete -f src/manifest/eks/deployment/application000001-deployment.yaml
-kubectl delete -f src/manifest/eks/deployment/region000001-deployment.yaml
-kubectl delete -f src/manifest/eks/deployment/root-deployment.yaml
-kubectl delete -f src/manifest/eks/configmap -R
-kubectl delete -f src/manifest/eks/services -R
+kubectl delete -f src/pxr-ver-1.0/delivery/manifest/eks/ingress/pxr-ingress.yaml
+kubectl delete -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/application000001-deployment.yaml
+kubectl delete -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/region000001-deployment.yaml
+kubectl delete -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/root-deployment.yaml
+kubectl delete -f src/pxr-ver-1.0/delivery/manifest/eks/configmap -R
+kubectl delete -f src/pxr-ver-1.0/delivery/manifest/eks/services -R
 
