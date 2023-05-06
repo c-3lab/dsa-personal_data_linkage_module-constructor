24346da5d487fc0ebf43a549294e27a55a0138e6
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2023-05-06 17:19:21 +0900

    extend timeout

--- 13_apply_k8s_resources.sh
+++ 13_apply_k8s_resources.sh
@@ -14,7 +14,7 @@ kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/region000001-d
 kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/deployment/application000001-deployment.yaml
 kubectl apply -f src/pxr-ver-1.0/delivery/manifest/eks/ingress/pxr-ingress.yaml
 
-kubectl wait -n $NAMESPACE --timeout=300s --for=condition=available deployment/root-api
-kubectl wait -n $NAMESPACE --timeout=300s --for=condition=available deployment/region000001-api
-kubectl wait -n $NAMESPACE --timeout=300s --for=condition=available deployment/application000001-api
+kubectl wait -n $NAMESPACE --timeout=600s --for=condition=available deployment/root-api
+kubectl wait -n $NAMESPACE --timeout=600s --for=condition=available deployment/region000001-api
+kubectl wait -n $NAMESPACE --timeout=600s --for=condition=available deployment/application000001-api
 
